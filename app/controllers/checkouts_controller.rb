c93fdc28849c1ea8fc8459836f207aac36ef834c
Agna Basil <yasirbasil5@gmail.com>
2025-12-02 10:17:56 +0530

    BugFix: minor changes and fixes

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -11,6 +11,11 @@ class CheckoutsController < ApplicationController
   end
 
   def create
+    if @cart.cart_items.empty?
+      redirect_to cart_path, alert: "Your cart is empty."
+      return
+    end
+
     @order = Order.new(order_params)
     @order.total_price = @cart.total_price
 
@@ -41,7 +46,7 @@ class CheckoutsController < ApplicationController
       session[:cart_id] = nil
 
       # Redirect to WhatsApp
-      phone_number = "7907542340" # Admin Number
+      phone_number = ENV.fetch("ADMIN_PHONE_NUMBER", "917907542340") # Admin Number
       whatsapp_url = "https://wa.me/#{phone_number}?text=#{CGI.escape(message)}"
 
       redirect_to whatsapp_url, allow_other_host: true
